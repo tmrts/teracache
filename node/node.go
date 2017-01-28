dcd2e9b9937c45cb7a70efb9ed247ad7d9b5dce2
Tamer Tas <contact@tmrts.com>
2017-01-28 17:19:11 +0300

    node: extract a new type for node addresses

--- node/node.go
+++ node/node.go
@@ -2,11 +2,21 @@ package node
 
 import "fmt"
 
+type Address string
+
+func (addr *Address) String() string {
+	return fmt.Sprint(*addr)
+}
+
 type Type struct {
-	Identity string
-	Address  string
+	identity string
+	address  Address
 }
 
 func (n *Type) Bytes() []byte {
-	return []byte(fmt.Sprintf("%s-%s", n.Identity, n.Address))
+	return []byte(fmt.Sprintf("%s-%s", n.identity, n.address))
+}
+
+func (n *Type) Address() Address {
+	return n.address
 }
