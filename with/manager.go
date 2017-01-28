3fcf932824f9df453426737cc9ed05b8138d8979
Tamer Tas <contact@tmrts.com>
2017-01-28 10:51:17 +0300

    with: add a context manager for read locks

--- with/manager.go
+++ with/manager.go
@@ -8,3 +8,11 @@ func Lock(l sync.Locker, fn func() error) (err error) {
 
 	return fn()
 }
+
+type ReadLocker interface {
+	RLocker() sync.Locker
+}
+
+func ReadLock(l ReadLocker, fn func() error) (err error) {
+	return Lock(l.RLocker(), fn)
+}
