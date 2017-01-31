c23a8dced2e4a222dc512bc6bf0e86c14fdce0b0
Tamer Tas <contact@tmrts.com>
2017-01-31 16:01:21 +0300

    cache: implement an LRU cache

--- cache/cache.go
+++ cache/cache.go
@@ -1,6 +1,9 @@
 package cache
 
-import "github.com/tmrts/hordecache/payload"
+import (
+	"github.com/hashicorp/golang-lru/simplelru"
+	"github.com/tmrts/hordecache/payload"
+)
 
 type Key string
 
@@ -10,4 +13,42 @@ type Interface interface {
 	Add(Key, payload.Payload)
 
 	Purge()
+}
+
+// EvictionNotice is invoked when an entry is evicted from the cache.
+type EvictionNotice func(Key, payload.Payload)
+
+type lru struct {
+	cache *simplelru.LRU
+}
+
+func NewLRU(size int, notify EvictionNotice) Interface {
+	if size < 0 {
+		panic("cache size must be non-negative!")
+	}
+
+	genericNotice := func(k, v interface{}) {
+		notify(k.(Key), v.(payload.Payload))
+	}
+
+	cache, _ := simplelru.NewLRU(size, genericNotice)
+
+	return &lru{cache}
+}
+
+func (c *lru) Add(k Key, v payload.Payload) {
+	_ = c.cache.Add(k, v)
+}
+
+func (c *lru) Get(k Key) (payload.Payload, bool) {
+	p, ok := c.cache.Get(k)
+	if !ok {
+		return nil, false
+	}
+
+	return p.(payload.Payload), true
+}
+
+func (c *lru) Purge() {
+	c.cache.Purge()
 }
